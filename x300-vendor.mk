12657993c34e7f461bf1fcf2245bc32cee47f9d7
John Reinel Caluag <johnrenielcaluag1009@gmail.com>
2016-06-10 08:15:22 +0800

    Update to x300

---
+++ x300-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherrymobile/x300/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherrymobile/x300/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherrymobile/x300/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherrymobile/x300/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherrymobile/x300/xbin,system/xbin)
+
